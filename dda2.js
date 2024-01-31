68267f7a6d532de45c5d82d6e3e7cf6d730923f3
Raghav B. Venkataramaiyer <bvraghav@gmail.com>
2024-01-31 15:24:14 +0530

    Update with DDA algorithm.

--- dda2.js
+++ dda2.js
@@ -25,13 +25,13 @@ flush({ctx, imData, x0, y0})
 // Draw a line
 // ----------------------------------------------------
 
-px = 87
-py = 27
-qx = 243
-qy = 124
-N = 125
+px = 243
+py = 124
+qx = 200
+qy = 27
+N = 1250
 
-const linePoints = collectPointsOnLine(px, py, qx, qy, N)
+const linePoints = collectPointsOnLineDda(px, py, qx, qy, N)
 setPixels(linePoints, imData, W, H)
 flush({ctx, imData, x0, y0})
 
@@ -39,6 +39,56 @@ flush({ctx, imData, x0, y0})
 // ----------------------------------------------------
 // Function Definitions
 // ----------------------------------------------------
+
+function collectPointsOnLineDda(px, py, qx, qy)  {
+  let dx, dy, inc, x, y
+
+  const S = []
+
+  dx = qx-px
+  dy = qy-py
+  if (Math.abs(dy) < Math.abs(dx)) {
+    // Slope is gentle
+    // |m| < 1
+
+    inc = Math.sign(dx)
+    x = px
+    y = py
+
+    while (inc * x <= inc * qx) {
+      S.push([Math.floor(x), Math.floor(y)])
+      x += inc
+      y += inc * (dy/dx)
+    }
+
+  } else {
+    
+    // Slope is steep
+    // |m| >= 1
+
+
+    inc = Math.sign(dy)
+    x = px
+    y = py
+
+    while (inc * y <= inc * qy) {
+      S.push([Math.floor(x), Math.floor(y)])
+      y += inc
+      x += inc * (dx/dy)
+    }
+
+  }
+
+  return S
+
+}
+
+function collectPointsOnLine2(px, py, qx, qy)  {
+  let N
+  N = Math.max(Math.abs(py-qy), Math.abs(px-qx))
+  N += 1
+  return collectPointsOnLine(px, py, qx, qy, N)
+}
 
 function collectPointsOnLine(px, py, qx, qy, N) {
   return range(1+N)
